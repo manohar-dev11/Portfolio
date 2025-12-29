8f7f15a30a7c7f29421dfd5995a03fa594d7b51f
Manohar <bandramanohar11@gmail.com>
2025-12-30 02:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,5 @@ def intro():
 def portfolio():
     return render_template("index.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 if __name__ == "__main__":
     app.run()
