0bb36c95385ccdf0c55e999022157e24db2ebe93
Manohar <bandramanohar11@gmail.com>
2025-12-30 02:32:37 +0530

    Changed Templates and some minor changes

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def intro():
-    return render_template("intro.html")
+    return render_template("intr.html")
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("index.html")
+    return render_template("inde.html")
 
 if __name__ == "__main__":
     app.run()
